32f742d4fbb857e736d8f6cd3b159440fffacceb
OWDSC <64771783+OWDSC@users.noreply.github.com>
2022-05-17 17:02:28 +0200

    gelbe Seiten funktioniert

--- harvester-gelbe-seiten_test.js
+++ harvester-gelbe-seiten_test.js
@@ -2,11 +2,15 @@ Feature('My First Test');
 
 Scenario('test something', async ({ I }) => {
     I.amOnPage('https://www.gelbeseiten.de/Suche/hundefutter/Mannheim');
+
     I.wait(5);
-    I.say('hi');
     I.click('Akzeptieren')
+
     I.wait(2);
     const htmlMitOeffnungszeit = await I.grabHTMLFrom('.oeffnungszeitKompakt')
+    const htmlMitAdresse = await I.grabHTMLFrom('.mod-AdresseKompakt')
 
     I.say(htmlMitOeffnungszeit)
+    I.say(htmlMitAdresse)
+    I.wait(3);
 });
