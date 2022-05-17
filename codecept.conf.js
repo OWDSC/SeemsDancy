47a538a43a2cb69ecb968e3054ff3a86e6d61db2
OWDSC <64771783+OWDSC@users.noreply.github.com>
2022-05-17 15:14:55 +0200

    Spengler Code

---
+++ codecept.conf.js
@@ -0,0 +1,18 @@
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'aktuelle-data-science-entwicklungen-2-wwi19dsab',
+  translation: 'de-DE'
+}
