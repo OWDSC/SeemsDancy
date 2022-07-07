732e438a95124e626caae2a995e441ba98e22b4f
Marius2311 <72920791+Marius2311@users.noreply.github.com>
2022-07-07 16:58:53 +0200

    Commit py file to exchange data between JS and Py

---
+++ data_Exchange_Js_And_Py/neu.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request, session
+from flask_cors import CORS
+#<strong>#Set up Flaskstrong>:
+app = Flask(__name__)
+#<strong>#Set up Flask to bypass CORSstrong>:
+cors = CORS(app)
+#Create the receiver API POST endpoint:
+import os
+ 
+
+receivedText = {}   
+
+@app.route("/receiver", methods=["POST"])
+def startCodeCept():
+   
+   print("Before Harvest")
+   os.system('npm run codeceptjs')          # start CodeceptJS
+   print("After Harvest")
+
+   readParsedData()
+   return True
+
+def readParsedData(): 
+   global receivedText
+
+   f = open("./test.txt", "r")                # read what was parsed by Codecept
+   receivedText = f.read()
+ 
+   print("Ergebnis in Python")
+   print(receivedText)
+   
+if __name__ == "__main__": 
+   app.run(debug=True)
