f37c418b07e234f74c71557fd32c9717bacfb11e
OWDSC <64771783+OWDSC@users.noreply.github.com>
2022-08-11 22:15:56 +0200

    Probleme mit Facebook

--- sendDataWithCodecept_test.js
+++ sendDataWithCodecept_test.js
@@ -36,31 +36,46 @@ Scenario('test something', async ({ I }) => {
     I.click('Suche nach')
 
 
-    I.wait(3);
+    I.wait(5);
     I.click('Veranstaltungen')
-    I.wait(3);
+    I.wait(5);
     //I.click({ xpath: '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div/div[1]/div/span/span'})
-    I.click('Party')
+    //I.click('Party')
 
 
 
 
     //Versuch mehrer Seiten nacheinander aufzurufen
-    locate('<span class="a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7 ltmttdrg g0qnabr5">Interessiert</span>').at(1)
-        .click('Interessiert')
+    //locate('Interessiert').at(1)
+    //I.click('//*[@id="mount_0_0_QM"]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div')
+    I.click('Interessiert')
+    I.wait(5)
+    //I.click('Interessiert')
     // second element
     //locate('#table td').at(2);
 
-    I.wait(3)
+    //I.wait(3)
     //I.click('Interessiert')
-    I.wait(3)
+    //I.wait(3)
+
+    //locate('data-imgperflogname="profileCoverPhoto"')
+        //var currentName = I.grabTextFrom('src=');
+
+    //var currentName = await I.grabTextFrom('//*[@id="mount_0_0_QM"]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div/div[2]/div/a/div/div/div/div/img');
+
+    //var currentName = await I.grabValueFrom('//*[@id="mount_0_0_QM"]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div/div[2]/div/a/div/div/div/div/img')
+    var currentName = await I.grabValueFrom('//*[@id="mount_0_0_QM"]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div/div[2]/div/a/div/div/div/div')
+    //var currentName = yield I.grabTextFrom('//input[@id="firstName"]');
+
+    //const content_facebook = await I.grabTextFrom('')
 
 
-    I.wait(3);
-	  console.log("SZENARIO VORBEI")
+    I.wait(20);
+	console.log(currentName)
 
     //const content = 'https://www.online-druck.biz/gestalten/events-veranstaltungen.html'    // put here the data that was scraped by the Bot (for example image-URL)
-    const content = 'https://www.carpediem.life/wp-content/uploads/2021/12/bewusstsein-schoene-bilder-pixabay.jpg'
+    //const content = 'https://www.carpediem.life/wp-content/uploads/2021/12/bewusstsein-schoene-bilder-pixabay.jpg'
+    const content = 'https://scontent-frt3-1.xx.fbcdn.net/v/t39.30808-6/298319074_6090255590988966_7551239222269917826_n.jpg?stp=dst-jpg_p180x540&_nc_cat=106&ccb=1-7&_nc_sid=340051&_nc_ohc=sFEbFpKu4GMAX8OSSp8&_nc_ht=scontent-frt3-1.xx&oh=00_AT_r8-69KxJFp2HF7ph8taGz_5TgeswLWkt8ja37rMSo3g&oe=62FA7840'
 
     fs.writeFile('./test.txt', content, err => {                            // write the parsed content to a local file
     if (err) {
